d3a0748584e4c160fee3ee9d1e3cff156a118bd2
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-25 16:31:37 +0530

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+    './projects/**/*.{html,ts}',
+    './node_modules/primeng/**/*.{js,ts}'
+  ],
+  plugins: [
+  ]
+};
+
