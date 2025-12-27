0bc3034714fc6460ca99c94f0a314de8a5a6843d
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-27 15:08:59 +0530

    created auth guard if user is not logged in than cannot access main route

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './routes/auth';
 import { mainRoutes } from './routes/main';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'auth', children: [...authRoutes] },
-  { path: '', children: [...mainRoutes] },
+  { path: '', children: [...mainRoutes] , canActivate: [authGuard]},
 ];
