bd38c5d3a08022be51ba498c3a1af70da3726ca3
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-28 01:00:09 +0530

    basic reactive form added

--- projects/admin/src/app/pages/login/login.ts
+++ projects/admin/src/app/pages/login/login.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { Auth } from '../../layout/auth/auth';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [Auth, ButtonModule],
+  imports: [Auth, ButtonModule, CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+    }
+  }
 }
