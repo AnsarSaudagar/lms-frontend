d5e8cd3e32839451d210016d92bde97f419e3d3e
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-27 23:24:37 +0530

    Created auth service

--- projects/admin/src/app/app.config.ts
+++ projects/admin/src/app/app.config.ts
@@ -9,12 +9,14 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { customPreset } from './customPreset';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
--- projects/admin/src/app/services/auth.service.ts
+++ projects/admin/src/app/services/auth.service.ts
@@ -1,8 +1,104 @@
-import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+
+interface AuthResponse {
+  accessToken: string;
+}
+
+interface StoredAuthData {
+  token: string;
+  expiresAt: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  isLoggedIn = signal(true);
+  private readonly TOKEN_KEY = 'token';
+  private readonly API_URL = environment.API_URL + '/auth';
+
+  private tokenSignal = signal<string | null>(null);
+  private expiresAtSignal = signal<number | null>(null);
+
+  readonly isLoggedIn = computed(() => {
+    const token = this.tokenSignal();
+    const expiresAt = this.expiresAtSignal();
+
+    return !!token && !!expiresAt && Date.now() < expiresAt;
+  });
+
+  private logoutTimer: any;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponse>(this.API_URL + '/login-admin', { email, password })
+      .subscribe({
+        next: (res: AuthResponse) => this.handleAuth(res.accessToken),
+        error: () => this.logout(),
+      });
+  }
+
+  autoLogin() {
+    const tokenData = localStorage.getItem(this.TOKEN_KEY);
+
+    if (!tokenData) return;
+
+    const data: StoredAuthData = JSON.parse(tokenData);
+
+    if (Date.now() >= data.expiresAt) {
+      this.logout();
+      return;
+    }
+
+    this.tokenSignal.set(data.token);
+    this.expiresAtSignal.set(data.expiresAt);
+
+    this.startLogoutTimer(data.expiresAt);
+  }
+
+  logout() {
+    this.clearTimer();
+    this.tokenSignal.set(null);
+    this.expiresAtSignal.set(null);
+    localStorage.removeItem(this.TOKEN_KEY);
+    this.router.navigate(['auth', 'login']);
+  }
+
+  getToken(): string | null {
+    return this.tokenSignal();
+  }
+
+  private handleAuth(token: string) {
+    const expiresAt = this.extractExpiration(token);
+
+    this.tokenSignal.set(token);
+    this.expiresAtSignal.set(expiresAt);
+
+    localStorage.setItem(this.TOKEN_KEY, JSON.stringify({ token, expiresAt }));
+
+    this.startLogoutTimer(expiresAt);
+  }
+
+  private extractExpiration(token: string): number {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload.exp * 100;
+  }
+
+  private startLogoutTimer(expiresAt: number) {
+    this.clearTimer();
+
+    const duration = expiresAt - Date.now();
+    this.logoutTimer = setTimeout(() => this.logout(), duration);
+  }
+
+  private clearTimer() {
+    if (this.logoutTimer) {
+      clearTimeout(this.logoutTimer);
+      this.logoutTimer = null;
+    }
+  }
 }
