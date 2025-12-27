cfa17e72f05c401c549a6d5302b7724a6e599419
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-27 15:11:36 +0530

    returning the router navigation with returnUrl

--- projects/admin/src/app/core/guards/auth-guard.ts
+++ projects/admin/src/app/core/guards/auth-guard.ts
@@ -6,8 +6,11 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(Auth);
   const router = inject(Router);
 
+  // If the user is not logged in than redirect to login page
   if (!authService.isLoggedIn()) {
-    router.navigate(['auth', 'login']);
+    return router.createUrlTree(['auth', 'login'], {
+      queryParams: { returnUrl: state.url },
+    });
   }
 
   return true;
