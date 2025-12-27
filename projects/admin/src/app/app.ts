07761c4ff3c443d986c2ee5d9dd8dc968ffd6091
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-27 14:35:40 +0530

    removed unneccessary imports

--- projects/admin/src/app/app.ts
+++ projects/admin/src/app/app.ts
@@ -1,17 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Button } from 'primeng/button';
-import { Card } from 'primeng/card';
-import { FloatLabel } from 'primeng/floatlabel';
-import { IconField } from 'primeng/iconfield';
-import { InputIcon } from 'primeng/inputicon';
-import { Common } from 'shared';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
