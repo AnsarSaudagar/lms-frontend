6b9759b7a34b654e3c3ad5eb44188a0e0347ae92
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-27 15:01:52 +0530

    added main layout

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './routes/auth';
+import { mainRoutes } from './routes/main';
 
 export const routes: Routes = [
-    {path: 'auth', children: [
-        ...authRoutes
-    ]}
+  { path: 'auth', children: [...authRoutes] },
+  { path: '', children: [...mainRoutes] },
 ];
---
+++ projects/admin/src/app/routes/main.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { Main } from "../layout/main/main";
+
+export const mainRoutes: Routes = [
+    {path: '', component: Main}
+];
