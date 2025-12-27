fbcb12039a51ef9f7238a5ef5bae839b0cd3dee0
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-27 23:56:24 +0530

    Login component attach with layout

---
+++ projects/admin/src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Auth } from '../../layout/auth/auth';
+
+@Component({
+  selector: 'app-login',
+  imports: [Auth],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
--- projects/admin/src/app/routes/auth.ts
+++ projects/admin/src/app/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Routes } from "@angular/router";
 import { Auth } from "../layout/auth/auth";
+import { Login } from "../pages/login/login";
 
 export const authRoutes: Routes = [
-  { path: 'login', component: Auth },
+  { path: 'login', component: Login },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
