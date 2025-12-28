c9cfa439f3ffff9b1a253a10d853626d34709970
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-28 21:51:40 +0530

    import changes

--- projects/admin/src/app/pages/test/test.ts
+++ projects/admin/src/app/pages/test/test.ts
@@ -4,7 +4,7 @@ import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-test',
-  imports: [Button, RouterLink, ButtonDirective],
+  imports: [Button, RouterLink],
   templateUrl: './test.html',
   styleUrl: './test.css',
 })
