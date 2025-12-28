4574a813bd8689175dc6838c25884fcbbdce23a0
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-28 21:30:16 +0530

    testing

--- projects/admin/src/app/pages/test/test.ts
+++ projects/admin/src/app/pages/test/test.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Button, ButtonDirective } from 'primeng/button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [Button, RouterLink, ButtonDirective],
   templateUrl: './test.html',
   styleUrl: './test.css',
 })
--- projects/admin/src/app/routes/main.ts
+++ projects/admin/src/app/routes/main.ts
@@ -2,7 +2,7 @@ import { Routes } from "@angular/router";
 import { Main } from "../layout/main/main";
 import { Home } from "../pages/home/home";
 import { Test } from "../pages/test/test";
-import { testLayoutResolver } from "../core/resolvers/layout.resolver";
+import { layoutResolver, testLayoutResolver } from "../core/resolvers/layout.resolver";
 
 export const mainRoutes: Routes = [
     {path: '', component: Home},
