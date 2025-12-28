5001ea4ee3372bca1a7bd462acbca959ee319bcc
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-28 20:26:35 +0530

    basic route changes

--- projects/admin/src/app/routes/main.ts
+++ projects/admin/src/app/routes/main.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { Main } from "../layout/main/main";
+import { Home } from "../pages/home/home";
 
 export const mainRoutes: Routes = [
-    {path: '', component: Main}
+    {path: '', component: Home},
 ];
