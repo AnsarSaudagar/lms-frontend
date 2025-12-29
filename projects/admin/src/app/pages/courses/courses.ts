0d0690bbdbc8a5526d0a72b3b5c4a16828e50a00
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-29 17:50:53 +0530

    course page header

--- projects/admin/src/app/pages/courses/courses.ts
+++ projects/admin/src/app/pages/courses/courses.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {  ButtonModule } from "primeng/button";
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './courses.html',
   styleUrl: './courses.css',
 })
