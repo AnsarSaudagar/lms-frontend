0d2fa8e147ec084d8e9cffa5111da2fa8e906805
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-29 14:40:59 +0530

    formatting changes

--- projects/admin/src/app/routes/main.ts
+++ projects/admin/src/app/routes/main.ts
@@ -4,8 +4,5 @@ import { Test } from "../pages/test/test";
 
 export const mainRoutes: Routes = [
     { path: '', component: Home },
-    {
-        path: 'test', component: Test,
-        // data: { layout: 'test' }
-    },
+    { path: 'test', component: Test },
 ];
