e0ce0b8f48fb71fcbac2e1ee4d993f64645fa5f7
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-31 20:31:39 +0530

    added quill for summnernote

--- projects/admin/src/app/core/components/courses/detail-form/detail-form.ts
+++ projects/admin/src/app/core/components/courses/detail-form/detail-form.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EditorModule } from 'primeng/editor';
 
 @Component({
   selector: 'app-detail-form',
-  imports: [],
+  imports: [EditorModule],
   templateUrl: './detail-form.html',
   styleUrl: './detail-form.css',
 })
 export class DetailForm {
-
+  text = '';
 }
