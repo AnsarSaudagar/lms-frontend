abd231d499857283495898534a5be6ce3ccee944
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-02 00:00:00 +0530

    generated a component for global loader and added code for it

--- projects/admin/src/app/app.ts
+++ projects/admin/src/app/app.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { ConfigureLayout } from './core/components/configure-layout/configure-layout';
+import { GlobalLoader } from './core/components/global-loader/global-loader';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, ConfigureLayout],
+  imports: [ CommonModule, ConfigureLayout, GlobalLoader],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
