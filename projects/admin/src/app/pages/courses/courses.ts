0efcbb0d4a40ca5ab10b783a1413366a840c6e74
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-03 20:50:09 +0530

    Component generated and basic div created

--- projects/admin/src/app/pages/courses/courses.ts
+++ projects/admin/src/app/pages/courses/courses.ts
@@ -3,10 +3,11 @@ import {  ButtonModule } from "primeng/button";
 import { RouterLink } from "@angular/router";
 import { CourseService } from '../../services/course.service';
 import { Course } from '../../models/course.model';
+import { CourseList } from '../../core/components/courses/course-list/course-list';
 
 @Component({
   selector: 'app-courses',
-  imports: [ButtonModule, RouterLink],
+  imports: [ButtonModule, RouterLink, CourseList],
   templateUrl: './courses.html',
   styleUrl: './courses.css',
 })
