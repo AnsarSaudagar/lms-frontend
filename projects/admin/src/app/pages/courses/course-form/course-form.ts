a96a00b296816135fee27ce48340aeb60c7cde82
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-04 13:05:33 +0530

    fetching course

--- projects/admin/src/app/pages/courses/course-form/course-form.ts
+++ projects/admin/src/app/pages/courses/course-form/course-form.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Navbar } from '../../../core/components/courses/navbar/navbar';
 import { PreviewCard } from '../../../core/components/courses/preview-card/preview-card';
 import { DetailForm } from '../../../core/components/courses/detail-form/detail-form';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../../../models/course.model';
+import { CourseService } from '../../../services/course.service';
 
 @Component({
   selector: 'app-course-form',
@@ -10,5 +13,21 @@ import { DetailForm } from '../../../core/components/courses/detail-form/detail-
   styleUrl: './course-form.css',
 })
 export class CourseForm {
+  courseData: Course | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private courseService: CourseService) { }
+
+  ngOnInit() {
+    const courseId: string | null = this.route.snapshot.paramMap.get('id');
+
+    if (courseId) {
+      this.courseService.getCourseById(courseId).subscribe({
+        next: (course: Course) => {
+          this.courseData = course;
+        }
+      });
+    }
+  }
 }
