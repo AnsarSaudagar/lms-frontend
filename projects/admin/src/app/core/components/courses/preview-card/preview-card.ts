12ff409ae8d8acb553639a8b8972b0fe21791afc
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-05 00:28:26 +0530

    topics and duration showing on preview and basic css changes

--- projects/admin/src/app/core/components/courses/preview-card/preview-card.ts
+++ projects/admin/src/app/core/components/courses/preview-card/preview-card.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { CourseService } from '../../../../services/course.service';
+import { DurationPipe } from 'shared';
 
 @Component({
   selector: 'app-preview-card',
-  imports: [],
+  imports: [DurationPipe],
   templateUrl: './preview-card.html',
   styleUrl: './preview-card.css',
 })
 export class PreviewCard {
+  courseData = computed(() => {
+    return this.courseService.selectedCourse();
+  });
+
+  courseDuration = computed(() => {
+    const course = this.courseService.selectedCourse();
+    if (!course) return 0;
+
+    let duration = 0;
+    course.topics.forEach(topic => {
+      duration += topic.duration;
+    })
+
+    return duration;
+  });
+
+  constructor(private courseService: CourseService) { }
 
 }
