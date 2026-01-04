9bf5e84bc97878c842b5de142d04f0fa77dc02ac
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-04 17:18:16 +0530

    added dynamic title in form and added icon for going back

--- projects/admin/src/app/core/components/courses/navbar/navbar.ts
+++ projects/admin/src/app/core/components/courses/navbar/navbar.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CourseService } from '../../../../services/course.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonModule],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
-  constructor(private courseService: CourseService){}
+  title : string = 'Create New Course';
+
+  constructor(private courseService: CourseService){
+    effect(() => {
+      const signalCourseData = this.courseService.selectedCourse();
+
+      if (!signalCourseData) return;
+      this.title = signalCourseData.title;
+    });
+  }
 
   onSaveCourse() {
     this.courseService.mainFormSubmit$.next();
