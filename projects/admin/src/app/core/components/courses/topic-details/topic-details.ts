a07e53cb7d2c37fade5ca630ca60b160e051e9a5
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-05 12:04:47 +0530

    created basic dialog for adding topics

--- projects/admin/src/app/core/components/courses/topic-details/topic-details.ts
+++ projects/admin/src/app/core/components/courses/topic-details/topic-details.ts
@@ -3,15 +3,17 @@ import { Component, computed } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CourseService } from '../../../../services/course.service';
 import {DurationPipe} from 'shared';
+import { DialogModule } from 'primeng/dialog';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-topic-details',
-  imports: [CommonModule, ButtonModule, DurationPipe],
+  imports: [CommonModule, ButtonModule, DurationPipe, DialogModule, TextareaModule],
   templateUrl: './topic-details.html',
   styleUrl: './topic-details.css',
 })
 export class TopicDetails {
-  
+  visible = false;
   topics = computed(() => {
     const course = this.courseService.selectedCourse();
     return course?.topics;
