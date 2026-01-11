b018f372383977d63d656f28745f9c6a225afc53
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-11 16:14:58 +0530

    changed login ui

--- projects/admin/src/app/pages/login/login.ts
+++ projects/admin/src/app/pages/login/login.ts
@@ -4,10 +4,21 @@ import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-
+import { CheckboxModule } from 'primeng/checkbox';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 @Component({
   selector: 'app-login',
-  imports: [ButtonModule, CommonModule, ReactiveFormsModule],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    InputGroupAddonModule,
+    InputGroupModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
