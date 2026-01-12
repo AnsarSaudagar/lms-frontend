5657a930f0cd2603ebd3cb519834fa4718d8a337
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-12 23:15:28 +0530

    displaying data

--- projects/admin/src/app/pages/error-logger/error-logger.ts
+++ projects/admin/src/app/pages/error-logger/error-logger.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { Tag } from 'primeng/tag'
+import { ErrorLogService } from '../../services/error-log.service';
 @Component({
   selector: 'app-error-logger',
   imports: [CommonModule, ButtonModule, Tag],
@@ -9,33 +10,44 @@ import { Tag } from 'primeng/tag'
   styleUrl: './error-logger.css',
 })
 export class ErrorLogger {
-selectedError: any = null;
+  selectedError: any = null;
 
   // Mock data from your DB
-  errorLogs = [
-    {
-      id: 'ERR-4002',
-      type: 'DatabaseConnectionError',
-      message: 'Connection pool exhausted while fetching user session.',
-      severity: 'danger',
-      timestamp: 'Just now',
-      userId: 'USR_882',
-      ip: '192.168.1.45',
-      browser: 'Chrome 120',
-      stack: 'Error: Connection pool limit reached\n  at Pool.acquire (pg-pool.js:145:12)\n  at Query.execute (db.service.ts:88:4)'
-    },
-    {
-      id: 'ERR-4003',
-      type: 'ReferenceError',
-      message: 'variable "courseId" is not defined',
-      severity: 'warning',
-      timestamp: '5 mins ago',
-      userId: 'USR_112',
-      ip: '10.0.0.8',
-      browser: 'Safari Mobile',
-      stack: 'ReferenceError: courseId is not defined\n  at CourseComponent.save (course.component.ts:122:15)'
-    }
-  ];
+  // errorLogs = [
+  //   {
+  //     id: 'ERR-4002',
+  //     type: 'DatabaseConnectionError',
+  //     message: 'Connection pool exhausted while fetching user session.',
+  //     severity: 'danger',
+  //     timestamp: 'Just now',
+  //     userId: 'USR_882',
+  //     ip: '192.168.1.45',
+  //     browser: 'Chrome 120',
+  //     stack: 'Error: Connection pool limit reached\n  at Pool.acquire (pg-pool.js:145:12)\n  at Query.execute (db.service.ts:88:4)'
+  //   },
+  //   {
+  //     id: 'ERR-4003',
+  //     type: 'ReferenceError',
+  //     message: 'variable "courseId" is not defined',
+  //     severity: 'warning',
+  //     timestamp: '5 mins ago',
+  //     userId: 'USR_112',
+  //     ip: '10.0.0.8',
+  //     browser: 'Safari Mobile',
+  //     stack: 'ReferenceError: courseId is not defined\n  at CourseComponent.save (course.component.ts:122:15)'
+  //   }
+  // ];
+  errorLogs : any = [];
+
+  constructor(private errorLogService: ErrorLogService){}
+
+  ngOnInit(){
+    this.errorLogService.getAllErrorLogs().subscribe({
+      next: (errors: any) => {
+        this.errorLogs = errors;
+      }
+    })
+  }
 
   getSeverity(severity: string): 'success' | 'info' | 'warn' | 'danger' | 'secondary' {
     switch (severity) {
