8fb7335df9b5600b25664685ef92d90b1e44f31e
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-17 19:23:25 +0530

    primeng setup

--- projects/learner/src/app/app.config.ts
+++ projects/learner/src/app/app.config.ts
@@ -1,12 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { customPreset } from './customPreset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: customPreset,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'none',
+            primary: 'blue'
+        },
+      },
+    }),
   ]
 };
