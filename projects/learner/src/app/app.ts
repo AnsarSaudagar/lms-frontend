467a81e56857b924767dd8de0d58417a751ab034
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-01-17 22:37:57 +0530

    using backend data to display

--- projects/learner/src/app/app.ts
+++ projects/learner/src/app/app.ts
@@ -4,6 +4,7 @@ import { ButtonModule } from 'primeng/button';
 import { RatingModule } from 'primeng/rating';
 import { TagModule } from 'primeng/tag';
 import { FormsModule } from '@angular/forms';
+import { CourseService } from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -14,19 +15,28 @@ import { FormsModule } from '@angular/forms';
 export class App {
   protected readonly title = signal('learner');
 
+  constructor(private courseService: CourseService){}
+
   courses: any[] = [
-    {
-      id: 1,
-      title: 'The Complete Angular Masterclass 2026',
-      instructor: 'Dr. Sarah Smith',
-      thumbnail: 'https://placehold.co/600x400/png',
-      rating: 4.8,
-      reviews: 12540,
-      price: 12.99,
-      originalPrice: 84.99,
-      badge: 'Bestseller'
-    },
-    // Add more mock objects here...
+    // {
+    //   id: 1,
+    //   title: 'The Complete Angular Masterclass 2026',
+    //   instructor: 'Dr. Sarah Smith',
+    //   thumbnail: 'https://placehold.co/600x400/png',
+    //   rating: 4.8,
+    //   reviews: 12540,
+    //   price: 12.99,
+    //   originalPrice: 84.99,
+    //   badge: 'Bestseller'
+    // },
+    // // Add more mock objects here...
   ];
 
+  ngOnInit(){
+    this.courseService.getAllCourses().subscribe({
+      next: (courses: any) => {
+        this.courses = courses;
+      }
+    });
+  }
 }
--- projects/learner/src/app/services/courses.service.ts
+++ projects/learner/src/app/services/courses.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
-  // private API_URL = 
+  private API_URL = environment.API_URL + '/courses';
+
+  constructor(private http: HttpClient){
+  }
+
+  getAllCourses(){
+    return this.http.get(this.API_URL);
+  }
 
 }
