793f1b1dbbf9550a8f34dc83f24c0520ce9433b9
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-07-04 13:03:31 +0530

    project card component created

---
+++ projects/learner/src/app/pages/core/project-card/project-card.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Project } from '../../../services/app-data.service';
+
+@Component({
+  selector: 'app-project-card',
+  imports: [],
+  templateUrl: './project-card.html',
+  styleUrl: './project-card.scss',
+})
+export class ProjectCard {
+  @Input() project : Project | null = null;
+}
