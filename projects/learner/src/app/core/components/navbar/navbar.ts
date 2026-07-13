74ae4c38c19b78fa3e803f2c8d50ab7414e2e89c
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-07-13 15:02:11 +0530

    navbar added wishlist and cart icon

--- projects/learner/src/app/core/components/navbar/navbar.ts
+++ projects/learner/src/app/core/components/navbar/navbar.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  private authService = inject(AuthService);
+  user = this.authService.currentUser;
+  wishlistCount = 2;
+  cartCount = 0;
+}
