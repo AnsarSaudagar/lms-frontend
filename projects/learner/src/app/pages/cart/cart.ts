be5da832d58fb0340466787a58e531e14162ee9e
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-07-28 22:20:53 +0530

    basic structure created for cards

--- projects/learner/src/app/pages/cart/cart.ts
+++ projects/learner/src/app/pages/cart/cart.ts
@@ -1,10 +1,13 @@
 import { Component, inject, signal } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import {Cart as CartModel} from '../../models/cart.model'
+import { CommonModule } from '@angular/common';
+import { CartItemsList } from './cart-items-list/cart-items-list';
+import { CartCheckoutCard } from './cart-checkout-card/cart-checkout-card';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule, CartItemsList, CartCheckoutCard],
   templateUrl: './cart.html',
   styleUrl: './cart.scss',
 })
